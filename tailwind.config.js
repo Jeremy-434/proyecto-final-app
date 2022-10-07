c253ae2ba6f07681ab203f707a649fc6b63c7ccd
NIXON0220 <108483622+NIXON0220@users.noreply.github.com>
2022-10-07 02:26:24 -0500

    add estructure design

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ module.exports = {
     extend: {
       width: {
         'logo': '12em',
-      }
+      },
+      colors:{
+        'textw': '#2F2F2F',
+        'bgcard':'#EDEDED',
+      },
+      screens: {
+        'xs': '320px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [],
